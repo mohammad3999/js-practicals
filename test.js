fa81faf9ac47ec33f8981e4f6bd7589c0b812277
Mohammadkha Pathan <67869789+mohammad3999@users.noreply.github.com>
2024-07-04 14:05:37 +0000

    conversion of dataa types

--- test.js
+++ test.js
@@ -1,2 +1,23 @@
-console.log("Mohammad")
-console.log("I am Full stack Developer")
+// console.log("Mohammad")
+// console.log("I am Full stack Developer")
+
+// const accountId = 1234
+// let accountEmail = "mohammad@gmail.com"
+// var accountPassword = "1234"
+// accountCity = "Pune"
+
+// // accountId = 2020
+// console.log(accountId)
+
+// accountEmail = "pathan@google.com"
+// accountPassword = "4545"
+// accountCity = "Sakri"
+
+// console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
